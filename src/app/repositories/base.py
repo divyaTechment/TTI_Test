9f255da07873babc34fa1a74187ff7bcab139d6d
Divya Kashyap <divya.kashyap@farragut.com>
2025-12-19 20:11:50 +0530

    xxx

--- src/app/repositories/base.py
+++ src/app/repositories/base.py
@@ -21,7 +21,7 @@ def getOutClass(self, id: int) -> Optional[ModelType]:
         """
         a=4
         b=6
-       
+        c=a+b
         return self.db.query(self.model).filter(self.model.id == id).first()
     
 
