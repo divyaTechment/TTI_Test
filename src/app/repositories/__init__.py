8b9b59798e81102c05f6d55822f364133a942d7c
Divya Kashyap <divya.kashyap@techment.com>
2026-01-05 21:56:30 +0530

    cleanup

--- src/app/repositories/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Repository layer package for database operations.
-"""
-from .base import BaseRepository
-from .user import UserRepository
-
-__all__ = ["BaseRepository", "UserRepository"]
--- src/app/schemas/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Pydantic schemas package.
-"""
-from .user import UserCreate, UserUpdate, UserResponse, UserLogin
-from .base import BaseSchema
-
-__all__ = ["UserCreate", "UserUpdate", "UserResponse", "UserLogin", "BaseSchema"]
