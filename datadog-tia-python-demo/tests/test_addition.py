9241cd1094eee4e39cd7b972bc7e7739ee5df628
Divya Kashyap <divya.kashyap@techment.com>
2026-01-06 12:16:34 +0530

    added test cases

--- datadog-tia-python-demo/tests/test_addition.py
+++ datadog-tia-python-demo/tests/test_addition.py
@@ -1,9 +1,12 @@
+import pytest
 from app.calculator import add
 
 
+@pytest.mark.unit
 def test_add_positive():
     assert add(1, 2) == 3
 
 
+@pytest.mark.unit
 def test_add_negative():
     assert add(-1, -1) == -2
--- datadog-tia-python-demo/tests/test_subtraction.py
+++ datadog-tia-python-demo/tests/test_subtraction.py
@@ -1,9 +1,12 @@
+import pytest
 from app.calculator import subtract
 
 
+@pytest.mark.unit
 def test_subtract_positive():
     assert subtract(5, 3) == 2
 
 
+@pytest.mark.unit
 def test_subtract_negative():
     assert subtract(0, 5) == -5
